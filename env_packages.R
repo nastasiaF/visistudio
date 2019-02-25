05637b2d815667b0b1f8472c5be5de5c9468c3de
Nastasia Fouret <nastasia.fouret@lojelis>
2019-02-25 21:58:20 +0100

    add env_packages.R : to install package

---
+++ env_packages.R
@@ -0,0 +1,14 @@
+## Install environment
+install.packages("shiny")
+install.packages("httpuv", "mime", "jsonlite", "xtable", "digest", "htmltools", "R6", "sourcetools", "later",
+, "promises", "tools", "crayon", "rlang")
+
+### 	INSTALL VISIELSE
+install.packages("Rcpp","utils" , "later" )
+install.packages("chron")
+install.packages("colorspace")
+install.packages("stringr")
+install.packages("Matrix")
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("ViSiElse")
